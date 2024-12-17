3ac98dc10fb95a15aa7b43af47ab22bb4f150574
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-12-17 14:57:31 -0300

    resources page and folders

---
+++ app/resources/page.jsx
@@ -0,0 +1,13 @@
+
+
+function Resources() { // Componentes normalmente usam PascalCase
+  return (
+    <div>
+      <h1 className="text-8xl text-center text-pink-500 pt-16">
+        Resources Page
+      </h1>
+    </div>
+  );
+}
+
+export default Resources; // Nome PascalCase e exportação padrão
